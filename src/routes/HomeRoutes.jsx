fcce81d10290c4924aecbf7442bd9a60b88e5d64
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-12 11:59:58 -0500

    add navbar n header

--- src/routes/HomeRoutes.jsx
+++ src/routes/HomeRoutes.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import NavBarHeader from '../components/tools/NavBarHeader';
 import NotFound from '../pages/NotFound';
 
 const HomePageTest = () => (
@@ -6,18 +8,17 @@ const HomePageTest = () => (
 		<h1>This is the Home Page test</h1>
 	</div>
 );
-const HomePage = () => (
-	<div>
-		<h1>This is the Home Page 2</h1>
-	</div>
-);
 
 export default function HomeRoutes() {
 	return (
-		<Routes>
-			<Route path='test' element={<HomePageTest />}></Route>
-			<Route path='/' element={<HomePage />}></Route>
-			<Route path='*' element={<NotFound />}></Route>
-		</Routes>
+		<>
+			<NavBar />
+			<NavBarHeader />
+			<Routes>
+				<Route path='test' element={<HomePageTest />}></Route>
+				<Route path='/' element={<HomePageTest />}></Route>
+				<Route path='*' element={<NotFound />}></Route>
+			</Routes>
+		</>
 	);
 }
