71429bb24fe5749a1549e1abf3f537c5f32488c9
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-14 18:43:56 -0500

    empty pages n initial color

---
+++ src/pages/attributes/Color.jsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/material';
+import AttributeCard from '../../components/cards/AttributeCard';
+
+export default function Color() {
+	const example = [
+		{ id: 1, name: 'red', active: false },
+		{ id: 2, name: 'red', active: false },
+		{ id: 3, name: 'red', active: false },
+	];
+	return (
+		<>
+			<Box style={{ display: 'flex', flexWrap: 'wrap', margin: '0.5rem' }}>
+				{example.map(exam => (
+					<AttributeCard attribute={exam} key={exam.id} />
+				))}
+			</Box>
+		</>
+	);
+}
