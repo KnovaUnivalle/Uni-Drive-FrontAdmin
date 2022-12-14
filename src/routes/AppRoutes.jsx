d5908a887799b8da1a78dda0070e52cf4cf06602
franklyn narvaez <franklyn.narvaez@correounivalle.edu.co>
2022-12-13 21:35:08 -0500

    delete local in app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,27 +5,6 @@ import Initial from '../pages/Initial';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 
-export function Local() {
-	return (
-		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-					<img src='/vite.svg' className='logo' alt='Vite logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
-	);
-}
-
 export default function AppRoutes() {
 	return (
 		<Routes>
@@ -34,19 +13,11 @@ export default function AppRoutes() {
 				path='home/*'
 				element={
 					<RouteGuard>
-					<Home />
+						<Home />
 					</RouteGuard>
 				}
 			></Route>
 			<Route path='/login' element={<Login />}></Route>
-			<Route
-				path='/local'
-				element={
-					<RouteGuard>
-						<Local />
-					</RouteGuard>
-				}
-			></Route>
 			<Route path='*' element={<NotFound />}></Route>
 		</Routes>
 	);
