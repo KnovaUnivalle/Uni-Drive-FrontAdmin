5cad736dadce39f5c7dc7f993c98fb069a150783
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-14 11:10:10 -0500

    Update NavDrawer

--- src/components/nav/NavDrawer.jsx
+++ src/components/nav/NavDrawer.jsx
@@ -1,35 +1,54 @@
 import { useTheme } from '@mui/material/styles';
 import {
-	Drawer,
 	List,
 	ListItem,
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	Tooltip,
 	useMediaQuery,
 } from '@mui/material';
-import AcUnitIcon from '@mui/icons-material/AcUnit';
+import NavBarHeader from '../tools/NavBarHeader';
+import { useNavigate } from 'react-router-dom';
+import Drawer from '../tools/Drawer';
 
-const lst = ['mango', 'pera', 'manzana'];
-export default function NavDrawer() {
+export default function NavDrawer({ elements, objectElements }) {
 	const theme = useTheme();
 	const isMobile = useMediaQuery(theme.breakpoints.up('md'));
+	const navigate = useNavigate();
 	return (
-		<>
-			<Drawer variant='permanent' open={!isMobile}>
-				<List>
-					{lst.map(elmt => (
-						<ListItem key={elmt}>
-							<ListItemButton>
-								<ListItemIcon>
-									<AcUnitIcon />
-								</ListItemIcon>
-								<ListItemText>{elmt}</ListItemText>
-							</ListItemButton>
-						</ListItem>
-					))}
-				</List>
-			</Drawer>
-		</>
+		<Drawer variant='permanent' open={isMobile} style={{ zIndex: '0' }} disa>
+			<NavBarHeader />
+			<List>
+				{elements.map(element => (
+					<ListItem key={element} sx={{ display: 'block' }} disablePadding>
+						<ListItemButton
+							onClick={() => navigate(objectElements[element].url)}
+							sx={{
+								minHeight: 52,
+								justifyContent: !isMobile ? 'initial' : 'center',
+								px: 2.5,
+							}}
+						>
+							<ListItemIcon
+								sx={{
+									minWidth: 0,
+									mr: open ? 3 : 'auto',
+									justifyContent: 'center',
+								}}
+							>
+								<Tooltip title={element}>
+									{objectElements[element].icon}
+								</Tooltip>
+							</ListItemIcon>
+							<ListItemText
+								primary={element}
+								sx={{ opacity: isMobile ? 1 : 0 }}
+							/>
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</Drawer>
 	);
 }
