3e7748aab36a6525dde40531d84a9a275941f8b9
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-14 11:10:39 -0500

    update HomeRoutes

--- src/routes/HomeRoutes.jsx
+++ src/routes/HomeRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import NavDrawer from '../components/nav/NavDrawer';
+import Attributes from '../pages/Attributes';
 import DashBoard from '../pages/DashBoard';
 import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
@@ -7,10 +7,10 @@ import Profile from '../pages/Profile';
 export default function HomeRoutes() {
 	return (
 		<Routes>
-			<Route path='profile' element={<Profile />}></Route>
-			<Route path='/' element={<DashBoard />}></Route>
-			<Route path='*' element={<NotFound />}></Route>
-			<Route path='attributes' element={<NavDrawer />}></Route>
+			<Route path='profile' element={<Profile />} />
+			<Route path='/' element={<DashBoard />} />
+			<Route path='*' element={<NotFound />} />
+			<Route path='attributes/*' element={<Attributes />} />
 		</Routes>
 	);
 }
