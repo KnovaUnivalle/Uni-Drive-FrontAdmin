3c4ec7a8dc6f833fc63460ab8d13e321e4f87abd
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-16 18:34:36 -0500

    integrate fetch colors

--- src/pages/attributes/Color.jsx
+++ src/pages/attributes/Color.jsx
@@ -1,17 +1,28 @@
 import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
 import AttributeCard from '../../components/cards/AttributeCard';
+import { useAuth } from '../../hooks/useAuth';
+import { getColors } from '../../services/Attributes.service';
 
 export default function Color() {
-	const example = [
-		{ id: 1, name: 'red', active: false },
-		{ id: 2, name: 'red', active: true },
-		{ id: 3, name: 'red', active: false },
-	];
+	const [colors, setColors] = useState([]);
+	const { token } = useAuth();
+
+	const loadColors = async key => {
+		const res = await getColors(key);
+		setColors(res);
+		console.log(res);
+	};
+
+	useEffect(() => {
+		loadColors(token);
+	}, [token]);
+
 	return (
 		<>
 			<Box style={{ display: 'flex', flexWrap: 'wrap', margin: '0.5rem' }}>
-				{example.map(exam => (
-					<AttributeCard attribute={exam} key={exam.id} />
+				{colors.map(color => (
+					<AttributeCard attribute={color} key={color.id} />
 				))}
 			</Box>
 		</>
