0204dbc13afaf457cb350a7795911ed57f6f0aa8
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2022-12-19 16:13:12 -0500

    upgrade city page

--- src/pages/attributes/City.jsx
+++ src/pages/attributes/City.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react';
+import AddAttribute from '../../components/buttons/AddAttribute';
+import AttributeCard from '../../components/cards/AttributeCard';
+import { useFetch } from '../../hooks/useFetch';
+
+const route = 'trip/city';
+
 export default function City() {
-	return <div>City</div>;
+	const { get } = useFetch();
+	const [cities, setCities] = useState([]);
+
+	const loadCity = async () => {
+		const res = await get(route);
+		setCities(res);
+	};
+
+	const setNewCity = data => {
+		setCities([...cities, data]);
+	};
+
+	useEffect(() => {
+		loadCity();
+	}, []);
+
+	return (
+		<>
+			<div style={{ display: 'flex', flexWrap: 'wrap', margin: '0.5rem' }}>
+				{cities.map(city => (
+					<AttributeCard attribute={city} key={city.id} route={route} />
+				))}
+			</div>
+			<AddAttribute route={route} addFunction={setNewCity} />
+		</>
+	);
 }
