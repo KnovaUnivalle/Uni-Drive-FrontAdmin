88e31da0ab55ed6c7c22ba59c3159fe7234adc59
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-10 12:04:11 -0500

    add routes Not Found

---
+++ src/utils/RoutesNotFound.jsx
@@ -0,0 +1,10 @@
+export const basicRoutes = [
+	{ link: '/', name: 'INICIO' },
+	{ link: '/home', name: 'HOME' },
+];
+
+export const attributeRoutes = [
+	{ link: '/home', name: 'HOME' },
+	{ link: '/home/attribute', name: 'ATRIBUTOS' },
+	{ link: '/home/attribute/color', name: 'COLORES' },
+];
