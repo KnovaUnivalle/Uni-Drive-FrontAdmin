ab8c0ffae61589c1363525ce54aa2dddec8f8c89
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-24 00:18:36 -0500

    add load

--- src/components/decks/VehicleDeck.jsx
+++ src/components/decks/VehicleDeck.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react';
 import { useFetch } from '../../hooks/useFetch';
 import VehicleCard from '../cards/VehicleCard';
+import Load from '../tools/Load';
 
 export default function VehicleDeck({ route }) {
 	const { get } = useFetch();
 	const [vehicles, setVehicles] = useState([]);
+	const [charging, setCharging] = useState(true);
 
 	const loadVehicles = async () => {
+		setCharging(true);
 		const res = await get(route);
 		if (res.status === 404) {
 			console.log('error');
 		} else {
-			setVehicles(await res.json());
+			const dataVehicle = await res.json();
+			setVehicles(dataVehicle);
+			setCharging(false);
 		}
 	};
 
 	useEffect(() => {
 		loadVehicles();
 	}, []);
+
+	if (charging) {
+		return <Load />;
+	}
 
 	return (
 		<>
